546c0d57ddcc22c8e4de132d463d430e155656c6
Jeremy Finzel <jfinzel@enova.com>
2018-10-24 10:57:46 -0500

    Fix tags bug

--- functions/unique_tags.sql
+++ functions/unique_tags.sql
@@ -3,14 +3,19 @@ CREATE OR REPLACE FUNCTION pgl_ddl_deploy.unique_tags()
  LANGUAGE plpgsql
 AS $function$
 BEGIN
-  IF NOT NEW.ddl_only_replication AND EXISTS (
+-- The current record is not a ddl_only config
+  IF NOT NEW.ddl_only_replication
+
+-- And there is another config with the same set_name and the same tags which is also not a ddl_only config
+    AND EXISTS (
     SELECT 1
     FROM pgl_ddl_deploy.set_configs
     WHERE id <> NEW.id
       AND set_name = NEW.set_name
-      AND NOT NEW.ddl_only_replication
+      AND NOT ddl_only_replication
       AND (create_tags && NEW.create_tags
-      OR drop_tags && NEW.drop_tags)) THEN
+      OR drop_tags && NEW.drop_tags))
+    THEN
     RAISE EXCEPTION $$Another set_config already exists for '%' with overlapping create_tags or drop_tags.
     Command tags must only appear once per set_name even if using multiple set_configs, unless you
     are using the ddl_only_replication setting.
